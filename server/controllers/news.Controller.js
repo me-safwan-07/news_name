33cdb5fa8abd795723e242c0feb0e9497556cbd2
Muhammed Safwan <167682973+me-safwan-07@users.noreply.github.com>
2024-10-31 18:54:21 +0530

    basic changes

--- server/controllers/news.Controller.js
+++ server/controllers/news.Controller.js
@@ -1,10 +1,22 @@
 import News from "../models/News.js";
 
 // Get All Blogs
+// In newsController.js
 export const getBlogs = async (req, res, next) => {
   try {
-    const blogs = await News.find({}).sort({ updatedAt: -1 });
-    res.status(200).json(blogs);
+    const { page = 1, limit = 10 } = req.query; // Default to page 1, 10 items per page
+    const blogs = await News.find({})
+      .sort({ updatedAt: -1 })
+      .skip((page - 1) * limit) // Skip the documents of previous pages
+      .limit(Number(limit)); // Limit to 'limit' documents
+    
+    const totalBlogs = await News.countDocuments(); // Count total documents
+    res.status(200).json({
+      totalBlogs,
+      currentPage: page,
+      totalPages: Math.ceil(totalBlogs / limit),
+      blogs
+    });
   } catch (err) {
     console.error("Error fetching blogs:", err);
     next(err);
