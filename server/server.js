4e91b6cf6e1fff1d257aee3c3ee493f9284df677
Muhammed Safwan <167682973+me-safwan-07@users.noreply.github.com>
2024-10-31 19:30:17 +0530

    db add

--- server/server.js
+++ server/server.js
@@ -27,7 +27,7 @@ app.use('/api/demo', demodata);
 
 // MongoDB Connection
 mongoose
-    .connect('mongodb+srv://mesafwan07:Muha_2005@cluster0.uvnxh.mongodb.net/news-website?retryWrites=true&w=majority&appName=Cluster0')
+    .connect('mongodb+srv://mesafwan07:Muha_2005@cluster0.uvnxh.mongodb.net/news-website?retryWrites=true&w=majority')
     .then(() => console.log('MongoDB connected...'))
     .catch((error) => console.error('Error connecting to MongoDB:', error.message));
 
